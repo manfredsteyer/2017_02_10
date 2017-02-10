ed16b5e5b1d1e7aa5e4d1758ca0506832f27c695
ManfredSteyer <manfred.steyer@gmx.net>
2017-02-10 09:48:37 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { AppState } from './app.service';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent { 
+}
